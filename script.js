5a992e49d74ac659efe8e1368a55d3ac6f48d9c5
Florencia <mfc.mdp86.gmail.com>
2023-06-03 23:19:56 -0300

    Modificaciones en el formulario

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let cantidadTickets = 0
+let tickets = 0
 let descuentoSeleccionado = 0
-let totalAPagar = 0
+let total = 0
 
 
-function getValueFromInput(){
-    cantidadTickets = document.getElementById('inputCantidad').value  //tomo el input "Cantidad" y lo meto en una variable
-    descuentoSeleccionado = document.getElementById('inputDescuento').value //tomo el input "Descuento" y lo meto en otra variable
-    console.log("Cantidad de tickets seleccionada: "+cantidadTickets)//muestro en consola
-    console.log("Descuento seleccionado: "+descuentoSeleccionado) //muestro en consola
-    totalAPagar = cantidadTickets*descuentoSeleccionado*200     //calulo valor total de los tickets con descuento y lo meto en una variable
-    document.getElementById('aPagar').innerText='$'+totalAPagar.toFixed(2)    //meto el resultado en el párrafo <p> con id="aPagar"
+function getValueFromResumen(){
+    tickets = document.getElementById('CantidadTickets').value  //tomo el input "Cantidad" y lo meto en una variable
+    descuentoSeleccionado = document.getElementById('Descuento').value //tomo el input "Descuento" y lo meto en otra variable
+    console.log("Cantidad de tickets seleccionada: "+ tickets)//muestro en consola
+    console.log("Descuento seleccionado: "+ descuentoSeleccionado) //muestro en consola
+    total = tickets*descuentoSeleccionado*200     //calulo valor total de los tickets con descuento y lo meto en una variable
+    document.getElementById('aPagar').innerText='$'+ total.toFixed(2)    //meto el resultado en el párrafo <p> con id="aPagar"
+}
+
+function borrar (){
+    document.getElementById('aPagar').innerText='';
 }
